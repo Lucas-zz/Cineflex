c0cc778980d5fdfe27ac7ae2c49853452745308d
Lucas <lucasazzollinivieira@gmail.com>
2021-12-14 01:03:45 -0300

    all layout done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+
+const index = document.querySelector(".root");
+ReactDOM.render(<App />, index);
