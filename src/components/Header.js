513e926edf3480c40dd6b18660d135a97f3dd62d
Lucas <lucasazzollinivieira@gmail.com>
2021-12-20 15:36:26 -0300

    back navigation button done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import backArrow from "../assets/back-arrow.svg";
+
 export default function Header() {
+    const navigate = useNavigate();
+
+    function Navigate() {
+        navigate(-1);
+    }
+
     return (
-        <header className="header">
-            CINEFLEX
-        </header>
+        <>
+            {window.location.pathname !== "/"
+                ?
+                <header className="header">
+                    <p>CINEFLEX</p>
+                    <img onClick={Navigate} src={backArrow} alt="back-arrow" />
+                </header>
+                :
+                <header className="header">
+                    <p>CINEFLEX</p>
+                </header>
+            }
+        </>
     );
 }
